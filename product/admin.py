8e76e3569eb25cf6f0dc021c309c38bd5578545c
huseyintekin <huseyintekin10@gmail.com>
2023-08-09 10:49:42 +0200

    fdy

--- product/admin.py
+++ product/admin.py
@@ -7,11 +7,23 @@ from .models import *
 # Product
 # ----------------------------
 class ProductAdmin(ModelAdmin):
-    # Tablo sutunlari
-    list_display = ["id", "name", "is_in_stock", "create_date", "update_date"]
-    list_editable = ["is_in_stock"]
-    list_display_links = ["id", "name"]
-    list_filter = ["is_in_stock", "create_date", "update_date"]
+    # Tablo sutunları (model field names)
+    list_display = ['id', 'name', 'is_in_stock', 'create_date', 'update_date']
+    # Tablo üzerinde güncelleyebilme:
+    list_editable = ['is_in_stock']
+    # Kayda gitmek için linkleme:
+    list_display_links = ['id', 'name']
+    # Filtreleme (arama değil):
+    list_filter = ['is_in_stock', 'create_date', 'update_date']
+    # Arama:
+    search_fields = ['id', 'name']
+    # Default Sıralama:
+    ordering = ['id'] # 'id' -> ASC, '-id' -> DESC
+    # Sayfa başına kayıt sayısı:
+    list_per_page = 20
+    # Tümünü göster yaparken max kayıt sayısı:
+    list_max_show_all = 200
+    date_hierarchy = 'create_date'
 
 
 
